98761c1d084e4308e679c26192b38d57e5757da0
Chris King-Parra <chris@kingparra.work>
2024-12-09 13:31:26 -0500

    Change data dir for mysql container

--- runmysql.sh
+++ runmysql.sh
@@ -2,7 +2,7 @@
 podman run --rm --detach \
   --name mariadb_devcontainer \
   -e MARIADB_ROOT_PASSWORD=easypass \
-  -v /var/home/chris/Projects/mastery-with-sql/mysql-data/:/var/lib/postgresql/mysql-data:Z \
+  -v ./mysql-data/:/var/lib/postgresql/mysql-data:Z \
   -p 8080:8080 \
   mariadb:latest
 
